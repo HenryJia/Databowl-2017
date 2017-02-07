c372970d96439a460e64f722a34dd2744d29f168
Hengjian Jia <henryjia18@gmail.com>
2017-02-07 20:29:32 +0000

    Add script to generate submissions

---
+++ models_lib.py
@@ -0,0 +1,55 @@
+import numpy as np
+import scipy
+import theano.tensor as T
+
+from keras.models import Model, Sequential
+from keras.layers import Input, Convolution3D, Convolution2D, MaxPooling3D, MaxPooling2D, Dense
+from keras.layers import Dropout, Reshape, Lambda, Flatten
+from keras.layers.advanced_activations import LeakyReLU, ELU
+from keras.callbacks import ModelCheckpoint
+
+def convnet3d(input_shape = (1, 64, 64, 64)):
+
+    model = Sequential()
+
+    model.add(Convolution3D(2, 3, 3, 3, border_mode = 'same', input_shape = (1, ) + input_shape[1:]))
+    model.add(ELU())
+    model.add(MaxPooling3D((2, 2, 2)))
+
+    model.add(Convolution3D(4, 3, 3, 3, border_mode = 'same'))
+    model.add(ELU())
+    model.add(MaxPooling3D((2, 2, 2)))
+
+    model.add(Convolution3D(8, 3, 3, 3, border_mode = 'same'))
+    model.add(ELU())
+    model.add(MaxPooling3D((2, 2, 2)))
+
+    model.add(Convolution3D(16, 3, 3, 3, border_mode = 'same'))
+    model.add(ELU())
+    model.add(MaxPooling3D((2, 2, 2)))
+
+    model.add(Convolution3D(32, 3, 3, 3, border_mode = 'same'))
+    model.add(ELU())
+    model.add(Convolution3D(32, 3, 3, 3, border_mode = 'same'))
+    model.add(ELU())
+    model.add(MaxPooling3D((2, 2, 2)))
+
+    model.add(Convolution3D(32, 3, 3, 3, border_mode = 'same'))
+    model.add(ELU())
+    model.add(Convolution3D(32, 3, 3, 3, border_mode = 'same'))
+    model.add(ELU())
+
+    model.add(Flatten())
+
+    model.add(Dense(200))
+    model.add(ELU())
+    model.add(Dropout(0.5))
+    model.add(Dense(200))
+    model.add(ELU())
+    model.add(Dropout(0.5))
+
+    model.add(Dense(1, activation = 'sigmoid'))
+
+    model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['binary_accuracy'])
+
+    return model
---
+++ submit.py
@@ -0,0 +1,61 @@
+from __future__ import print_function
+import os
+import random
+import time
+from collections import OrderedDict
+import dicom
+
+import numpy as np
+import scipy
+import theano.tensor as T
+
+from preprocess import normalize, zero_center
+import models_lib as ml
+
+directory = '/home/data/henry/Databowl-2017/'
+
+filenames = []
+
+with open(directory + 'stage1_sample_submission.csv') as f:
+    for i, line in enumerate(f):
+        if i > 0:
+            patient_id, _ = line.split(',')
+            filenames += [patient_id]
+
+print('Loading model')
+
+model = ml.convnet3d()
+model.load_weights('weights.hdf5')
+
+print('Read data')
+
+x = []
+for fn in filenames:
+    data = np.load(directory + 'stage1/' + fn + '/img_segment_fill.npy').astype(np.float32)
+    data = zero_center(normalize(data))
+    data = scipy.ndimage.interpolation.zoom(data, np.array((64, 64, 64)) / np.array(data.shape), order = 1)
+
+    x += [np.expand_dims(data, axis = 0)]
+
+x = np.expand_dims(np.concatenate(x, axis = 0), axis = 1)
+
+print('Run model')
+
+out = model.predict(x)
+
+print('Save data')
+
+f_out = open('submission.csv', 'w')
+f_round = open('submission_round.csv', 'w')
+
+f_out.write('id,cancer\n')
+f_round.write('id,cancer\n')
+
+for i, fn in enumerate(filenames):
+    f_out.write(fn + ',' + str(float(out[i])) + '\n')
+    f_round.write(fn + ',' + str(float(np.round(out[i]))) + '\n')
+
+f_out.close()
+f_round.close()
+
+print('All done :)')
